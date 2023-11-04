dcb3cd1bde571138a5a9f3a2e1a299d283c4c6d9
Lysander <lysander6@gmail.com>
2023-11-04 22:26:56 +0100

    Day 7 part 2 solution

--- day_07/src/bin/day_07.rs
+++ day_07/src/bin/day_07.rs
@@ -1,7 +1,7 @@
 use std::{env, fs};
 
 use anyhow::Context;
-use day_07::{count_containing_bags, Problem};
+use day_07::{count_contained_bags, count_containing_bags, Problem};
 
 fn main() -> Result<(), anyhow::Error> {
     let input_path = env::args().nth(1).context("missing path argument")?;
@@ -10,6 +10,9 @@ fn main() -> Result<(), anyhow::Error> {
 
     let containing_bags_count = count_containing_bags(&p, "shiny gold")?;
     println!("Part 1: {containing_bags_count}");
+
+    let contained_bags_count = count_contained_bags(&p, "shiny gold")?;
+    println!("Part 2: {contained_bags_count}");
 
     Ok(())
 }
--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -9,7 +9,9 @@ use regex::Regex;
 #[derive(Debug)]
 pub struct Problem {
     name_to_idx: HashMap<String, usize>,
-    // idx_to_name: Vec<String>,
+    #[allow(dead_code)]
+    idx_to_name: Vec<String>,
+    contains: Vec<Vec<(usize, usize)>>,
     contained_by: Vec<Vec<(usize, usize)>>,
 }
 
@@ -23,6 +25,7 @@ impl FromStr for Problem {
         let mut next_bag_idx = 0usize;
         let mut name_to_idx: HashMap<String, usize> = HashMap::new();
         let mut idx_to_name: Vec<String> = Vec::new();
+        let mut contains: Vec<Vec<(usize, usize)>> = Vec::new();
         let mut contained_by: Vec<Vec<(usize, usize)>> = Vec::new();
 
         for line in s.lines() {
@@ -49,6 +52,7 @@ impl FromStr for Problem {
             if containing_bag_idx == idx_to_name.len() {
                 // new bag
                 idx_to_name.push(containing_bag_name.to_string());
+                contains.push(Vec::new());
                 contained_by.push(Vec::new());
             }
 
@@ -66,16 +70,19 @@ impl FromStr for Problem {
                     // new bag
                     // TODO: move to lambda function
                     idx_to_name.push(contained_bag_name.to_string());
+                    contains.push(Vec::new());
                     contained_by.push(Vec::new());
                 }
 
+                contains[containing_bag_idx].push((contained_bag_idx, contained_times));
                 contained_by[contained_bag_idx].push((containing_bag_idx, contained_times));
             }
         }
 
         Ok(Problem {
             name_to_idx,
-            // idx_to_name,
+            idx_to_name,
+            contains,
             contained_by,
         })
     }
@@ -116,6 +123,34 @@ pub fn count_containing_bags(p: &Problem, bag_name: &str) -> Result<usize, anyho
     Ok(containers.len())
 }
 
+fn count_contained(contains: &Vec<Vec<(usize, usize)>>, bag_idx: usize) -> usize {
+    let contained_bags = &contains[bag_idx];
+
+    if contained_bags.is_empty() {
+        return 0;
+    }
+
+    contained_bags
+        .iter()
+        .map(|&(bag_idx, count)| count + count * count_contained(contains, bag_idx))
+        .sum()
+}
+
+/// # Errors
+///
+/// Returns error if bag with `bag_name` does not exist
+pub fn count_contained_bags(p: &Problem, bag_name: &str) -> Result<usize, anyhow::Error> {
+    let Problem {
+        name_to_idx,
+        contains,
+        ..
+    } = p;
+
+    let bag_idx = name_to_idx.get(bag_name).context("bag name not found")?;
+
+    Ok(count_contained(contains, *bag_idx))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -131,14 +166,35 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.";
 
+    const TEST_INPUT_NESTED: &str = "\
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.";
+
     #[test]
     fn test_problem_parser() {
         assert!(TEST_INPUT.parse::<Problem>().is_ok());
     }
 
     #[test]
-    fn test_name() {
+    fn test_count_containing_bags() {
         let p: Problem = TEST_INPUT.parse().unwrap();
         assert_eq!(count_containing_bags(&p, "shiny gold").unwrap(), 4);
+    }
+
+    #[test]
+    fn test_count_contained_bags() {
+        let p: Problem = TEST_INPUT.parse().unwrap();
+        assert_eq!(count_contained_bags(&p, "shiny gold").unwrap(), 32);
+    }
+
+    #[test]
+    fn test_count_contained_bags_deeply_nested() {
+        let p: Problem = TEST_INPUT_NESTED.parse().unwrap();
+        assert_eq!(count_contained_bags(&p, "shiny gold").unwrap(), 126);
     }
 }
