c6db573233b40c3df0b7ae857ae570c78db41be7
Lysander <lysander6@gmail.com>
2023-11-12 17:17:36 +0100

    Day 11 part 1 solution

---
+++ day_11/src/lib.rs
@@ -0,0 +1,160 @@
+#![allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
+use std::str::FromStr;
+
+use anyhow::bail;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Space {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+#[derive(Debug)]
+pub struct Problem {
+    map: Vec<Vec<Space>>,
+}
+
+impl FromStr for Problem {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let map = s
+            .lines()
+            .map(|l| {
+                l.chars()
+                    .map(|c| match c {
+                        '.' => Ok(Space::Floor),
+                        'L' => Ok(Space::Empty),
+                        '#' => Ok(Space::Occupied),
+                        _ => bail!("unknown character"),
+                    })
+                    .collect::<Result<_, _>>()
+            })
+            .collect::<Result<_, _>>()?;
+
+        Ok(Problem { map })
+    }
+}
+
+fn neighbour_offsets(i: usize, j: usize, max_i: usize, max_j: usize) -> &'static [(isize, isize)] {
+    match (i, j) {
+        (0, 0) => &[(0, 1), (1, 0), (1, 1)],
+        (0, j) if j == max_j => &[(0, -1), (1, 0), (1, -1)],
+        (0, _) => &[(0, -1), (0, 1), (1, -1), (1, 0), (1, 1)],
+        (i, 0) if i == max_i => &[(-1, 0), (-1, 1), (0, 1)],
+        (_, 0) => &[(-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)],
+        (i, j) if i == max_i && j == max_j => &[(0, -1), (-1, 0), (-1, -1)],
+        (i, _) if i == max_i => &[(0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1)],
+        (_, j) if j == max_j => &[(0, -1), (-1, 0), (1, 0), (-1, -1), (1, -1)],
+        (_, _) => &[
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, 1),
+            (1, -1),
+        ],
+    }
+}
+
+fn run(input: &[Vec<Space>]) -> (Vec<Vec<Space>>, bool) {
+    let max_i = input.len() - 1;
+    let max_j = input[0].len() - 1;
+
+    let mut output = input.to_vec();
+    let mut changed = false;
+
+    for i in 0..=max_i {
+        for j in 0..=max_j {
+            match &input[i][j] {
+                Space::Floor => {}
+                seat @ (Space::Empty | Space::Occupied) => {
+                    let occupied_seats_count = neighbour_offsets(i, j, max_i, max_j)
+                        .iter()
+                        .map(|&(dx, dy)| ((i as isize + dx) as usize, (j as isize + dy) as usize))
+                        .filter_map(|(x, y)| {
+                            if input[x][y] == Space::Occupied {
+                                Some((x, y))
+                            } else {
+                                None
+                            }
+                        })
+                        .count();
+
+                    match seat {
+                        Space::Empty => {
+                            if occupied_seats_count == 0 {
+                                output[i][j] = Space::Occupied;
+                                changed = true;
+                            }
+                        }
+                        Space::Occupied => {
+                            if occupied_seats_count >= 4 {
+                                output[i][j] = Space::Empty;
+                                changed = true;
+                            }
+                        }
+                        Space::Floor => unreachable!(),
+                    }
+                }
+            }
+        }
+    }
+
+    (output, changed)
+}
+
+fn count_occupied_seats(input: &[Vec<Space>]) -> usize {
+    input
+        .iter()
+        .map(|row| row.iter().filter(|&s| *s == Space::Occupied).count())
+        .sum()
+}
+
+fn run_until_stable_state(input: Vec<Vec<Space>>) -> Vec<Vec<Space>> {
+    let mut map = input;
+
+    loop {
+        let (output, changed) = run(&map);
+        map = output;
+
+        if !changed {
+            return map;
+        }
+    }
+}
+
+#[must_use]
+pub fn solve_part_1(p: Problem) -> usize {
+    let Problem { map } = p;
+    let map = run_until_stable_state(map);
+
+    count_occupied_seats(&map)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL";
+
+    #[test]
+    fn test_solve_part_1() {
+        let p: Problem = TEST_INPUT.parse().unwrap();
+
+        assert_eq!(solve_part_1(p), 37);
+    }
+}
