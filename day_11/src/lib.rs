b973ac630e7aeb4dbecc6c2108ccd99cae392710
Lysander <lysander6@gmail.com>
2023-11-12 17:57:54 +0100

    Day 11 visible seats map

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use anyhow::bail;
 
@@ -127,6 +127,48 @@ fn run_until_stable_state(input: Vec<Vec<Space>>) -> Vec<Vec<Space>> {
     }
 }
 
+fn make_visible_seats_map(input: &[Vec<Space>]) -> HashMap<(usize, usize), Vec<(usize, usize)>> {
+    let mut map = HashMap::new();
+    let max_i = input.len() - 1;
+    let max_j = input[0].len() - 1;
+
+    for i in 0..=max_i {
+        for j in 0..=max_j {
+            if input[i][j] == Space::Floor {
+                continue;
+            }
+
+            for offset in neighbour_offsets(i, j, max_i, max_j) {
+                let mut nseat = (i as isize + offset.0, j as isize + offset.1);
+
+                while 0 <= nseat.0
+                    && nseat.0 <= max_i as isize
+                    && 0 <= nseat.1
+                    && nseat.1 <= max_j as isize
+                {
+                    match input[nseat.0 as usize][nseat.1 as usize] {
+                        Space::Floor => {
+                            nseat.0 += offset.0;
+                            nseat.1 += offset.1;
+                        }
+                        Space::Empty | Space::Occupied => {
+                            map.entry((i, j))
+                                .and_modify(|v: &mut Vec<(usize, usize)>| {
+                                    v.push((nseat.0 as usize, nseat.1 as usize));
+                                })
+                                .or_insert(vec![(nseat.0 as usize, nseat.1 as usize)]);
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    map
+}
+
 #[must_use]
 pub fn solve_part_1(p: Problem) -> usize {
     let Problem { map } = p;
@@ -151,10 +193,59 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL";
 
+    const TEST_INPUT_VISIBLE_SEATS_1: &str = "\
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....";
+
+    const TEST_INPUT_VISIBLE_SEATS_2: &str = "\
+.............
+.L.L.#.#.#.#.
+.............";
+
+    const TEST_INPUT_VISIBLE_SEATS_3: &str = "\
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.";
+
     #[test]
     fn test_solve_part_1() {
         let p: Problem = TEST_INPUT.parse().unwrap();
 
         assert_eq!(solve_part_1(p), 37);
+    }
+
+    #[test]
+    fn test_make_visible_seats_map_1() {
+        let p: Problem = TEST_INPUT_VISIBLE_SEATS_1.parse().unwrap();
+        let map = make_visible_seats_map(&p.map);
+
+        assert_eq!(map[&(4, 3)].len(), 8);
+    }
+
+    #[test]
+    fn test_make_visible_seats_map_2() {
+        let p: Problem = TEST_INPUT_VISIBLE_SEATS_2.parse().unwrap();
+        let map = make_visible_seats_map(&p.map);
+
+        assert_eq!(map[&(1, 1)], vec![(1, 3)]);
+    }
+
+    #[test]
+    fn test_make_visible_seats_map_3() {
+        let p: Problem = TEST_INPUT_VISIBLE_SEATS_3.parse().unwrap();
+        let map = make_visible_seats_map(&p.map);
+
+        assert!(!map.contains_key(&(3, 3)));
     }
 }
